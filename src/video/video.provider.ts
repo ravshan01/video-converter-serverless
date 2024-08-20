27353a55e2f2a1679aba5ed4c10ff004a84aeed3
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-08-20 16:39:54 +0500

    implemented VideoService.convert

--- src/video/video.provider.ts
+++ src/video/video.provider.ts
@@ -1,5 +1,25 @@
+import { Readable } from 'node:stream';
+
 export const VIDEO_PROVIDER_KEY = 'VIDEO_PROVIDER';
 
 export interface IVideoProvider {
   isVideoExtension(path: string): boolean;
+  convert(
+    video: Buffer | ArrayBuffer | Uint8Array | Readable,
+    options: IVideoProviderConvertOptions,
+  ): Readable;
+}
+
+export interface IVideoProviderConvertOptions {
+  container: string;
+  codec?: string;
+  audioCodec?: string;
+  /**
+   * WIDTHxHEIGHT
+   * @example 1280x720
+   */
+  size?: string;
+  bitrate?: string | number;
+  fps?: number | string;
+  preset?: string;
 }
--- src/video/video.service.ts
+++ src/video/video.service.ts
@@ -1,13 +1,44 @@
+import { PassThrough, Readable } from 'node:stream';
+
+import * as ffmpeg from 'fluent-ffmpeg';
 import { lookup } from 'mime-types';
 
 import { Injectable } from '@nestjs/common';
 
-import { IVideoProvider } from './video.provider';
+import { bufferToStream } from '../common/utils/buffer-to-stream.util';
+import { IVideoProvider, IVideoProviderConvertOptions } from './video.provider';
 
 @Injectable()
 export class VideoService implements IVideoProvider {
   isVideoExtension(path: string) {
     const mimeType = lookup(path);
     return mimeType ? mimeType.includes('video') : false;
+  }
+
+  convert(
+    video: Buffer | ArrayBuffer | Uint8Array | Readable,
+    options: IVideoProviderConvertOptions,
+  ): Readable {
+    const stream =
+      video instanceof Readable ? video : bufferToStream(Buffer.from(video));
+    const passThrough = new PassThrough();
+
+    const command = ffmpeg(stream).outputOptions(
+      [
+        // '-qscale:v 4',
+        `-f ${options.container}`,
+        options.codec ? `-vcodec ${options.codec}` : '',
+        options.audioCodec ? `-acodec ${options.audioCodec}` : '',
+        options.size ? `-s ${options.size}` : '',
+        options.bitrate ? `-b:v ${options.bitrate}` : '',
+        options.fps ? `-r ${options.fps}` : '',
+        options.preset ? `-preset ${options.preset}` : '',
+        '-movflags +faststart', // moves file metadata to the beginning of the file
+      ].filter(Boolean),
+    );
+
+    command.pipe(passThrough, { end: true });
+
+    return passThrough;
   }
 }
