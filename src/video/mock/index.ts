091badc1d881b6442d412ec0aa12aa20dc5db683
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-08-20 14:52:29 +0500

    add mock videos

---
+++ src/video/mock/index.ts
@@ -0,0 +1,13 @@
+import { join } from 'node:path';
+
+export const MOCK_VIDEOS = [
+  {
+    path: join(__dirname, 'air.mp4'),
+  },
+  {
+    path: join(__dirname, 'field.mp4'),
+  },
+  {
+    path: join(__dirname, 'street.mp4'),
+  },
+];
