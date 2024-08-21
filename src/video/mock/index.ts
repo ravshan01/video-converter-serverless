a515c8cef099ec4ecd96b408fc423b5ae057fd3f
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-08-21 15:58:29 +0500

    add video-without-audio.avi

--- src/video/mock/index.ts
+++ src/video/mock/index.ts
@@ -3,17 +3,21 @@ import { join } from 'node:path';
 export const MOCK_VIDEOS = {
   WithoutAudio: {
     MP4: {
-      name: 'video-without-audio',
+      name: 'video-without-audio-mp4',
       path: join(__dirname, 'video-without-audio.mp4'),
     },
     WebM: {
-      name: 'video-without-audio',
+      name: 'video-without-audio-webm',
       path: join(__dirname, 'video-without-audio.webm'),
+    },
+    AVI: {
+      name: 'video-without-audio-avi',
+      path: join(__dirname, 'video-without-audio.avi'),
     },
   },
   WithAudio: {
     MP4: {
-      name: 'video-with-audio',
+      name: 'video-with-audio-mp4',
       path: join(__dirname, 'video-with-audio.mp4'),
     },
   },
