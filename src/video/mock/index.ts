19e20ebef8e9e57407dfb00af8123c94355b3555
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-08-21 15:39:21 +0500

    update MOCK_VIDEOS

--- src/video/mock/index.ts
+++ src/video/mock/index.ts
@@ -3,14 +3,17 @@ import { join } from 'node:path';
 export const MOCK_VIDEOS = {
   WithoutAudio: {
     MP4: {
+      name: 'video-without-audio',
       path: join(__dirname, 'video-without-audio.mp4'),
     },
     WebM: {
+      name: 'video-without-audio',
       path: join(__dirname, 'video-without-audio.webm'),
     },
   },
   WithAudio: {
     MP4: {
+      name: 'video-with-audio',
       path: join(__dirname, 'video-with-audio.mp4'),
     },
   },
